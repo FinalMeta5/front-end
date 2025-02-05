a715d18def5e1ec7342e3838b031d2a62b885644
eunhye <dmsp2377@naver.com>
2025-02-05 19:24:48 +0900

    Design : 기본 레이아웃 반응형으로 초안 구성

---
+++ hiapp/src/components/commons/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header phone-fix ">
+    <img class="icon-image " src="/src/assets/images/BURURUNG_WHITE.png">
+  </header>
+</template>
+  
+  <script>
+  export default {
+    name: "HeaderComponent",
+  };
+  </script>
+  
+  <style scoped>
+  .header {
+    height: 60px;
+    background-color: #4192ff;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 24px;
+    font-weight: bold;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1000;
+  }
+
+  .icon-image {
+    width: 80px;
+    height: 50px;
+  }
+  </style>
---
+++ hiapp/src/components/layout/RightScreenComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="aside-screen">
+    <div class="container">
+        오른쪽에 내용 꾸미기
+    </div>
+    <p class="bururung-copy">&copy; 2025 HIFIVE. All Rights Reserved.</p>
+      <ul class="footer-links">
+        <li><a href="https://github.com/FinalMeta5" target="_blank"><i class="bi bi-github"></i> HIFIVE GITHUB</a></li>
+        <li><a href="#"><i class="bi bi-youtube"></i> HIFIVE YOUTUBE</a></li>
+      </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "LeftScreenComponent",
+};
+</script>
+
+<style scoped>
+@import "../../assets/style/aside-common.css";
+</style>
