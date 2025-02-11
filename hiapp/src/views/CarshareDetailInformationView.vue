245e308a86da608889814db34040615c1ed55416
nayeon990315 <99skdus@naver.com>
2025-02-11 16:23:56 +0900

    Fix: Firebase yml 파일 수정

--- hiapp/src/views/CarshareDetailInformationView.vue
+++ hiapp/src/views/CarshareDetailInformationView.vue
@@ -5,8 +5,8 @@
             <DriverInfoComponent :driver-id="driverId"/>
             <CarInfoComponent :driver-id="driverId"/>
             <DrivingInfoComponent :driver-id="driverId" :carShareRegiId="carShareRegiId"/>
-            <button>예 약 제발</button>
-            <button>취 소 제발</button>
+            <button>예 약 꼬옥</button>
+            <button>취 소 꼬옥</button>
         </div>
     </div>
 </template>
