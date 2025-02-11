612d3b50bd675bc95dd32d028985ae09ea66631c
nayeon990315 <99skdus@naver.com>
2025-02-11 15:57:45 +0900

    Fix: Firebase 버그 수정

--- hiapp/src/views/CarshareDetailInformationView.vue
+++ hiapp/src/views/CarshareDetailInformationView.vue
@@ -5,8 +5,8 @@
             <DriverInfoComponent :driver-id="driverId"/>
             <CarInfoComponent :driver-id="driverId"/>
             <DrivingInfoComponent :driver-id="driverId" :carShareRegiId="carShareRegiId"/>
-            <button>예 약</button>
-            <button>취 소</button>
+            <button>예 약 2</button>
+            <button>취 소 2</button>
         </div>
     </div>
 </template>
