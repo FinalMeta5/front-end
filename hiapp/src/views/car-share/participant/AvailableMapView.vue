f8ea0140d2ac4f8091d8745a5f63ec0b7bfd42c8
nayeon990315 <99skdus@naver.com>
2025-02-16 21:53:36 +0900

    Modify: 백엔드 배포 주소로 변경

--- hiapp/src/views/car-share/participant/AvailableMapView.vue
+++ hiapp/src/views/car-share/participant/AvailableMapView.vue
@@ -144,7 +144,7 @@ export default {
     // 차량 목록 데이터 백엔드에 요청
     async fetchCarList() {
       try {
-        const response = await authAxios.get("/api/carshare/registration/available-list");
+        const response = await axios.get("/api/carshare/registration/available-list");
         if (response.status === 200 && response.data) {
           this.carList = response.data;
 
